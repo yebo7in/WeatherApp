e2b359191563e409828acdabb15314d96cee74b4
yebo7in <166575644+yebo7in@users.noreply.github.com>
2024-11-03 17:37:05 -0800

    Update Forecast.js

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,18 +1,18 @@
-// src/components/Forecast.js
+
 import React from 'react';
-import './Forecast.css'; // Import CSS for styling
+import './Forecast.css'; 
 
 const Forecast = ({ data, unit }) => {
   return (
-    <div className="forecast-container"> {/* Add a class for styling */}
+    <div className="forecast-container"> {}
       {data.map((forecast) => {
-        // Calculate the temperature based on the unit
+        
         const temperature = unit === 'metric' 
           ? forecast.main.temp 
           : (forecast.main.temp * 9/5) + 32;
 
         return (
-          <div key={forecast.dt} className="forecast-item"> {/* Add class for each item */}
+          <div key={forecast.dt} className="forecast-item"> {}
             <p>{new Date(forecast.dt * 1000).toLocaleDateString()}</p>
             <p>
               Temperature: {Math.round(temperature)}° {unit === 'metric' ? 'C' : 'F'}
